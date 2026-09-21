9af71c773a1db5563a97949b63addb885ea73283
game_fighter_efn_1 <88023556+wargamere@users.noreply.github.com>
2026-09-21 13:04:43 +0200

    Update ukol.py

--- ukol.py
+++ ukol.py
@@ -1,11 +1,14 @@
 import random
+pokusy = 0
 cislo = random.randint(1, 69)
 while True:
-    uzivatelcislo = int(input("uhádni číslo 1-69:"))
+    uzivatelcislo = int(input("uhádni číslo 1-100:"))
     if uzivatelcislo > cislo:
         print("Moc vysoké")
+        pokusy = pokusy + 1
     elif uzivatelcislo < cislo:
         print("Moc nízké")
+        pokusy = pokusy + 1
     else:
         print("Správně!")
         break
